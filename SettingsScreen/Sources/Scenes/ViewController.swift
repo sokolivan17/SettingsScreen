5208a813aed490eb3e3a4f7ac749f005a15a3cb4
Ivan Sokol <sokolivan34@mail.ru>
2023-01-31 23:08:39 +0300

    Add table view.
    Add table view with system cell.

--- SettingsScreen/Sources/Scenes/ViewController.swift
+++ SettingsScreen/Sources/Scenes/ViewController.swift
@@ -9,12 +9,79 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private lazy var labelText = [
+        "Уведомление",
+        "Звуки, тактильные сигналы",
+        "Фокусирование",
+        "Экранное время",
+    ]
+
+    private lazy var cellImage = [
+        UIImage(systemName: "bell.badge"),
+        UIImage(systemName: "speaker.wave.3"),
+        UIImage(systemName: "moon.fill"),
+        UIImage(systemName: "hourglass"),
+    ]
+
+    private lazy var cellImageColor = [
+        UIColor.systemRed,
+        UIColor.systemPink,
+        UIColor.systemIndigo,
+        UIColor.systemIndigo,
+    ]
+
+    // MARK: - Outlets
+
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .grouped)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.dataSource = self
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+
+    // MARK: - Lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        view.backgroundColor = .red
+        view.backgroundColor = .white
+        title = "Настройки"
+        navigationController?.navigationBar.prefersLargeTitles = false
+        setupHierarchy()
+        setupLayout()
     }
 
+    // MARK: - Setup
 
+    private func setupHierarchy() {
+        view.addSubview(tableView)
+    }
+
+    private func setupLayout() {
+        NSLayoutConstraint.activate([
+            tableView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ])
+    }
+}
+
+// MARK: - Extension
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        labelText.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.accessoryType = UITableViewCell.AccessoryType.disclosureIndicator
+        cell.textLabel?.text = labelText[indexPath.row]
+        cell.imageView?.image = cellImage[indexPath.row]
+        cell.imageView?.backgroundColor = cellImageColor[indexPath.row]
+        cell.imageView?.tintColor = .white
+        return cell
+    }
 }
 
